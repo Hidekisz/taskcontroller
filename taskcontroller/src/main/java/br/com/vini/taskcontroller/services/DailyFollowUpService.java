aa530a57dedde048e9b83ad54baacd40da5ff75b
vnhideki <vinihicosta@gmail.com>
2024-10-22 02:41:41 -0300

    Mudança de escopo

--- taskcontroller/src/main/java/br/com/vini/taskcontroller/services/DailyFollowUpService.java
+++ taskcontroller/src/main/java/br/com/vini/taskcontroller/services/DailyFollowUpService.java
@@ -14,29 +14,37 @@ public class DailyFollowUpService {
     public PdfFollowUpResponse gerarPdfToday(LocalDateTime date) {
 //        Consulta banco de dados com date
 //        retorna json com lugar da imagem e data
-
-
-
-
+//        criar service para gerar o pdf em array byte[]
+//        usar json como datasource da criação do pdf jasper
 
         PdfFollowUpResponse pdfFollowUpResponse = new PdfFollowUpResponse();
+//        byte[] pdf = chamadaServiceGeracaoPdf;
         byte[] pdf = new byte[1];
         pdfFollowUpResponse.setPdf(pdf);
         return pdfFollowUpResponse;
     }
 
     public PdfFollowUpResponse gerarPdfDayX(ConsultDateRequest consultDateRequest) {
-        //Consulta banco de dados com consultDateRequest.date()
-        //     retorna json com lugar da imagem e data
+//        Consulta banco de dados com consultDateRequest.date();
+//        retorna json com lugar da imagem e data
+//        criar service para gerar o pdf em array byte[]
+//        usar json como datasource da criação do pdf jasper
+
         PdfFollowUpResponse pdfFollowUpResponse = new PdfFollowUpResponse();
+//        byte[] pdf = chamadaServiceGeracaoPdf;
         byte[] pdf = new byte[1];
         pdfFollowUpResponse.setPdf(pdf);
         return pdfFollowUpResponse;
     }
 
     public PdfFollowUpResponse gerarListaPdf() {
+//        Consulta banco de dados para trazer uma lista de todos os acompanhamentos
+//        retorna json com lugar da imagem no sistema (temporário) e data
+//        criar service para gerar o pdf em array byte[]
+//        usar json como datasource da criação do pdf jasper
 
         PdfFollowUpResponse pdfFollowUpResponse = new PdfFollowUpResponse();
+//        byte[] pdf = chamadaServiceGeracaoPdf;
         byte[] pdf = new byte[1];
         pdfFollowUpResponse.setPdf(pdf);
         return pdfFollowUpResponse;
